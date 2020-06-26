42ec6ca0a1c65052c770dcde8b2db58d2a737f6f
RafaBort <rafaelafonseca.bort2019ri@gmail.com>
2020-06-25 22:33:00 -0300

    exercicios resolvidos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um número:");
+            String  sinal = Console.ReadLine();
+            Decimal Sinal2 = Convert.ToDecimal(sinal);
+            Console.WriteLine(Math.Sign(Sinal2));
+
+            Console.WriteLine("se for 1 o número é positivo");
+            Console.WriteLine("se for -1 o número é negativo");
+            Console.WriteLine("se for 0 o número é nulo, é zero");
+
+        }
+    }
+}
